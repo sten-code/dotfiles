b3f38797abe86e3018e360324e0f726816abce13
sten-code <stentechnology2005@gmail.com>
2024-04-19 13:05:25 +0200

    Fixed nvchad

---
+++ .config/nvim/lua/mappings.lua
@@ -0,0 +1,10 @@
+require "nvchad.mappings"
+
+-- add yours here
+
+local map = vim.keymap.set
+
+-- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+map("n", ";", ":", { desc = "CMD enter command mode" })
+map("n", "<leader>db", "<cmd> DapToggleBreakpoint <CR>", { desc = "Add breakpoint at line" })
+map("n", "<leader>dr", "<cmd> DapContinue <CR>", { desc = "Start or continue the debugger" })
