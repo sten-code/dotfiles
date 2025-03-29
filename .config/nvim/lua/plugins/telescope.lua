c43dde30c01e076af3e82c6a4c35448299e67692
sten-code <stentechnology2005@gmail.com>
2025-03-29 12:19:26 +0100

    namu config

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -25,16 +25,16 @@ return {
 			require("telescope").load_extension("ui-select")
 		end,
 	},
-	{
-		"andrew-george/telescope-themes",
-		config = function()
-			require("telescope").load_extension("themes")
-			vim.keymap.set(
-				"n",
-				"<leader>th",
-				":Telescope themes<CR>",
-				{ noremap = true, silent = true, desc = "Theme Switcher" }
-			)
-		end,
-	},
+	-- {
+	-- 	"andrew-george/telescope-themes",
+	-- 	config = function()
+	-- 		require("telescope").load_extension("themes")
+	-- 		vim.keymap.set(
+	-- 			"n",
+	-- 			"<leader>th",
+	-- 			":Telescope themes<CR>",
+	-- 			{ noremap = true, silent = true, desc = "Theme Switcher" }
+	-- 		)
+	-- 	end,
+	-- },
 }
