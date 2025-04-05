05b5d3e8345e77c8a9bc06acea6b17cebe460028
sten-code <stentechnology2005@gmail.com>
2025-04-05 22:52:45 +0200

    Add session lens to auto-session

--- .config/nvim/lua/plugins/auto-session.lua
+++ .config/nvim/lua/plugins/auto-session.lua
@@ -1,10 +1,17 @@
 return {
 	"rmagatti/auto-session",
 	lazy = false,
-
-	opts = {
-		suppressed_dirs = { "~/", "~/coding", "~/Downloads", "/", "~/dotfiles" },
-		auto_restore = false,
-		-- log_level = 'debug',
-	},
+	config = function()
+		require("auto-session").setup({
+			suppressed_dirs = { "~/", "~/coding", "~/Downloads", "/", "~/dotfiles" },
+			auto_restore = false,
+			session_lens = {
+				buftypes_to_ignore = {},
+				load_on_setup = true,
+				theme_conf = { border = true },
+				previewer = false,
+			},
+		})
+		vim.keymap.set("n", "<leader>fa", require("auto-session.session-lens").search_session, { noremap = true })
+	end,
 }
