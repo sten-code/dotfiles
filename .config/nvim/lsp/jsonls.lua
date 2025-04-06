c92ed11dae26cc49039b15bd49bf2a5fca269c3f
sten-code <stentechnology2005@gmail.com>
2025-04-06 22:21:29 +0200

    Added --stdio to json lsp

--- .config/nvim/lsp/jsonls.lua
+++ .config/nvim/lsp/jsonls.lua
@@ -1,6 +1,6 @@
 ---@type vim.lsp.Config
 return {
-	cmd = { "vscode-json-language-server" },
+	cmd = { "vscode-json-language-server", "--stdio" },
 	filetypes = { "json", "jsonc" },
 	root_markers = {
 		".git",
