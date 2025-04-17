5d9d024e5bedb47124d0b56e5e535eab83a641ff
sten-code <stentechnology2005@gmail.com>
2025-04-17 21:06:23 +0200

    Add image plugin for showing images within the terminal inside neovim

---
+++ .config/nvim/lua/plugins/image.lua
@@ -0,0 +1,15 @@
+return {
+	-- lazy.nvim
+	{
+		"folke/snacks.nvim",
+		---@type snacks.Config
+		opts = {
+			image = {
+				doc = {
+					max_width = 50,
+					max_height = 25,
+				},
+			},
+		},
+	},
+}
